34fcf87c226b3d5c4cf661468cc95237f3233b56
Farad Alam <farad.dev@gmail.com>
2025-05-30 01:07:19 +0600

    sitemap updated

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,3 +1,5 @@
+import blogPost from '@/public/posts/blogPosts'
+
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: "https://new-born-baby-care.vercel.app",
@@ -9,5 +11,21 @@ module.exports = {
       changefreq: "weekly",
       priority: 0.8,
     };
+  },
+  additionalPaths: async (config) => {
+    // 🔥 Fetch dynamic routes (e.g., blog posts)
+    // const res = await fetch("https://your-api.com/api/posts"); // Replace with your API endpoint
+    // const posts = await res.json(); // Must return array like [{ slug: 'baby-care' }, ...]
+
+
+
+    return blogPost.map((post) => {
+      return {
+        loc: `${config.siteUrl}/blog/${post.slug}`,
+        changefreq: "weekly",
+        priority: 0.8,
+        lastmod: new Date().toISOString(),
+      };
+    });
   },
 };
